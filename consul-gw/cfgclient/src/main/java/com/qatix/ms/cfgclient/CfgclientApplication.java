8b89ce2cd7fb4499f411640ba5ba1280e123de3a
Xiaojun Tang <tangxiaojun@checheweike.com>
2019-01-08 00:41:31 +0800

    add turbine

--- consul-gw/cfgclient/src/main/java/com/qatix/ms/cfgclient/CfgclientApplication.java
+++ consul-gw/cfgclient/src/main/java/com/qatix/ms/cfgclient/CfgclientApplication.java
@@ -2,13 +2,15 @@ package com.qatix.ms.cfgclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CfgclientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CfgclientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CfgclientApplication.class, args);
+    }
 
 }
 
